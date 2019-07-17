d012f733dfd3c7576a7765f16511b63652f0a1f9
tjr226 <tjr226@gmail.com>
2019-07-17 13:36:45 -0400

    MVP complete

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,10 +3,10 @@ console.log("in scheme model");
 module.exports = {
     find,
     findById,
-    // findSteps,
-    // add,
-    // update,
-    // remove
+    findSteps,
+    add,
+    update,
+    remove
 }
 
 // resolves to an array of schemes
@@ -20,4 +20,37 @@ function findById(id) {
     return db('schemes')
         .where({ id })
         .first();
+}
+
+// expects a scheme ID
+function findSteps(id) {
+    return db('steps')
+        .join('schemes', 'schemes.id', 'steps.scheme_id')
+        .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+        .where('schemes.id', id)
+        .orderBy('steps.step_number')
+}
+
+// returns the object just added to DB
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme)
+        .then(schemes => {
+            return findById(schemes[0]);
+        })
+}
+
+// update
+function update(changes, id) {
+    return db('schemes')
+        .where({ id })
+        .update(changes)
+}
+
+
+// remove
+function remove(id) {
+    return db('schemes')
+        .where('id', id)
+        .del();
 }
